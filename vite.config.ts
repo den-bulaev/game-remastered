7ac4d99d8f49cbce2e950ae28161d2b5bc959151
den-bulaev <denisbulaev7@gmail.com>
2024-08-18 00:48:21 +0300

    vite dynamic env config added

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,31 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: "/den-bulaev-cube-game.netlify.app",
-  plugins: [react()],
+// export default defineConfig({
+//   // base: "/den-bulaev-cube-game.netlify.app",
+//   plugins: [react()],
 
-  server: {
-    host: "localhost", // Change 'localhost'
-    port: 3000, // Change the port number from 5173 to 3000 or any port you prefer
-  },
+//   server: {
+//     host: "localhost",
+//     port: 3000,
+//   },
+// });
+
+export default defineConfig(({ mode }) => {
+  if (mode === "production") {
+    return {
+      // base: "/",
+      plugins: [react()],
+    };
+  }
+
+  return {
+    base: "/den-bulaev-cube-game.netlify.app",
+    plugins: [react()],
+
+    server: {
+      host: "localhost",
+      port: 3000,
+    },
+  };
 });
