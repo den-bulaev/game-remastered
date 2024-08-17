22ae688fbae64826071e6b96566f11c5812f74ef
den-bulaev <denisbulaev7@gmail.com>
2024-08-18 01:54:35 +0300

    comments removed

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
-// export default defineConfig({
-//   // base: "/den-bulaev-cube-game.netlify.app",
-//   plugins: [react()],
-
-//   server: {
-//     host: "localhost",
-//     port: 3000,
-//   },
-// });
-
 export default defineConfig(({ mode }) => {
   if (mode === "production") {
     return {
-      // base: "https://den-bulaev-cube-game.netlify.app",
       plugins: [react()],
     };
   }
 
   return {
-    // base: "/den-bulaev-cube-game.netlify.app",
+    // base: modify base url,
     plugins: [react()],
 
     server: {
