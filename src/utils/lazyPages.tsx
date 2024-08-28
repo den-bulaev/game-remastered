da2bf64fd7f4b24ac988e76011ece6c0a03921e8
den-bulaev <denisbulaev7@gmail.com>
2024-08-28 16:27:36 +0300

    unnecessary removed

--- src/utils/lazyPages.tsx
+++
@@ -1,5 +0,0 @@
-import { lazy } from "react";
-
-export const LazyAuthorization = lazy(() => import("../pages/Authorization"));
-export const LazyGreeting = lazy(() => import("../pages/Greeting"));
-export const LazyGame = lazy(() => import("../pages/Game"));
