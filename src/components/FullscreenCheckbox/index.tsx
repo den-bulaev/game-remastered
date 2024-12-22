b6a71eb70d1d705906e736c555a151d48b8edae0
den-bulaev <denisbulaev7@gmail.com>
2024-12-22 19:03:40 +0200

    fullscreen fixed

--- src/components/FullscreenCheckbox/index.tsx
+++ src/components/FullscreenCheckbox/index.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent } from "react";
+import { ChangeEvent, useEffect, useState } from "react";
 import { ELocalStorageKeys } from "../../utils/localStorageKeys";
 
 interface ICustomCheckboxProps {
@@ -10,17 +10,42 @@ const FullscreenCheckbox: React.FC<ICustomCheckboxProps> = ({
   id,
   labelText,
 }) => {
+  const [isFullscreen, setIsFullscreen] = useState(false);
+
+  useEffect(() => {
+    document.addEventListener("fullscreenchange", changeFullscreen);
+
+    if (!isFullscreen) {
+      setIsFullscreen(!!localStorage.getItem(ELocalStorageKeys.IS_FULLSCREEN));
+    }
+
+    return () => {
+      document.removeEventListener("fullscreenchange", changeFullscreen);
+    };
+  }, []);
+
+  const changeFullscreen = () => {
+    if (document.fullscreenElement) {
+      setIsFullscreen(true);
+      localStorage.setItem(ELocalStorageKeys.IS_FULLSCREEN, "true");
+    } else {
+      setIsFullscreen(false);
+      localStorage.removeItem(ELocalStorageKeys.IS_FULLSCREEN);
+    }
+  };
+
   const handleChangeIsFullscreen = (e: ChangeEvent<HTMLInputElement>) => {
     if (document.documentElement.requestFullscreen) {
       if (e.target.checked) {
         document.documentElement.requestFullscreen();
-        localStorage.setItem(ELocalStorageKeys.IS_FULLSCREEN, "true");
+        setIsFullscreen(true);
       } else {
         document
           .exitFullscreen()
+          .then(() => {
+            setIsFullscreen(false);
+          })
           .catch(() => console.error("Failed to exit fullscreen"));
-
-        localStorage.removeItem(ELocalStorageKeys.IS_FULLSCREEN);
       }
     }
   };
@@ -30,7 +55,7 @@ const FullscreenCheckbox: React.FC<ICustomCheckboxProps> = ({
       <input
         className="styled-checkbox"
         id={id}
-        defaultChecked={!!localStorage.getItem(ELocalStorageKeys.IS_FULLSCREEN)}
+        checked={isFullscreen}
         type="checkbox"
         onChange={handleChangeIsFullscreen}
       ></input>
